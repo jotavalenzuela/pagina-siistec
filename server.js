7284f9c349f770df49571171b9f1e28d1cc89fd0
jotavalenzuela <jota.valenzuela.roman@gmail.com>
2016-09-30 16:17:38 -0300

    no funciona el slider

--- server.js
+++ server.js
@@ -23,16 +23,28 @@ app.get('/about', function(req, res) {
 //locacion page
 app.get('/locate', function(req, res) {
     res.render('pages/locate')
-})
+});
+//Contact page
+app.get('/contact', function(req, res) {
+    res.render('pages/contact')
+});
+//Services page
+app.get('/services', function(req, res) {
+    res.render('pages/services')
+});
 
-function redirectUnmatched(req, res) {
-  res.render('pages/index')
-}
 
 //todo lo que no este que vaya a home ejemplo "8080/adfasdf"
+function redirectUnmatched(req, res) {
+  res.render('pages/index')
+};
+
 app.get('*', function(req, res){
   res.redirect('/');
 });
+
+app.use ('/pages/scripts', express.static('static'));
+
 
 app.listen(8080);
 console.log('8080 is the magic port');
